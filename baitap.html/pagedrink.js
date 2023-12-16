9effc10d911ec39890bf02f972adc21faeca10a2
Nhung0907 <nhungnguyenthi2197@gmail.com>
2023-12-16 20:58:57 +0700

    16/12

---
+++ baitap.html/pagedrink.js
@@ -0,0 +1,34 @@
+const raw_drink =[
+    {
+        name: "Hoa quả dầm sữa chua ",
+        price: 40000,
+        image: "https://images.foody.vn/res/g3/23502/s400x400/9116dabc-008f-4122-b233-59f1cc75ee64.jpeg"
+    },
+    {
+        name: "Sinh tố bơ ",
+        price: 35000,
+        image: "https://images.foody.vn/res/g3/23502/s400x400/517cb933-3c85-4d12-bbe9-f92f8cb8a8a1.jpg",
+    }
+    ,{
+        name: "Nước cam ép ",
+        price: 40000,
+        image: "https://images.foody.vn/res/g100005/1000048733/s400x400/3843adb7-ba9c-4286-bbf9-eb606fea-4c8e87f3-220317092218.jpeg"
+    },
+    {
+        name: "Nước chanh tươi  ",
+        price: 30000,
+        image: "https://images.foody.vn/res/g100005/1000048733/s400x400/ab5a230c-3f9b-4820-b876-5255b16b-7f9fa049-220318135029.jpeg",
+    },
+    {
+        name: "Nước ép cần tây dứa  ",
+        price: 45000,
+        image: "https://images.foody.vn/res/g100005/1000048733/s400x400/1a856299-87f1-4228-ac00-b8a19b7a-4e5ee500-220426115333.jpeg",
+    },
+    {
+        name: "Nước bí đao   ",
+        price: 45000,
+        image: "https://images.foody.vn/res/g116/1153241/prof/s640x400/foody-upload-api-foody-mobile-an-0199cfff-221012181609.jpeg",
+    },
+
+]
+localStorage.setItem("drinks",JSON.stringify(raw_drink));
