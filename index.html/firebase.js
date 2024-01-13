0127502467c79bde54d0980a7fb3e45308fe4abc
Nhung0907 <nhungnguyenthi2197@gmail.com>
2024-01-13 19:21:58 +0700

    13/1

--- index.html/firebase.js
+++ index.html/firebase.js
@@ -1,18 +1,18 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth ,GoogleAuthProvider,signInWithPopup}  from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCrTxxdlzDVzAS9fgUwkdETMpDTF9s4jZg",
-    authDomain: "nhung0907-1532a.firebaseapp.com",
-    projectId: "nhung0907-1532a",
-    storageBucket: "nhung0907-1532a.appspot.com",
-    messagingSenderId: "411523488198",
-    appId: "1:411523488198:web:b559bcab43345ae6503ac7",
-    measurementId: "G-R93321MFW2"
+  apiKey: "AIzaSyCrTxxdlzDVzAS9fgUwkdETMpDTF9s4jZg",
+  authDomain: "nhung0907-1532a.firebaseapp.com",
+  projectId: "nhung0907-1532a",
+  storageBucket: "nhung0907-1532a.appspot.com",
+  messagingSenderId: "411523488198",
+  appId: "1:411523488198:web:b559bcab43345ae6503ac7",
+  measurementId: "G-R93321MFW2"
 };
 
 // Initialize Firebase
@@ -21,8 +21,8 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-const login =() => {
-    signInWithPopup(auth, provider)
+const login = () => {
+  signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -31,12 +31,12 @@ const login =() => {
       const user = result.user;
       // IdP data available using getAdditionalUserInfo(result)
       // ...
-      localStorage.setItem("user",JSON.stringify(user));
+      localStorage.setItem("user", JSON.stringify(user));
       alert("login successful")
-      window.location.href="/index.html/indexs.html"
+      window.location.href = "/baitap.html/main.html"
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -47,5 +47,5 @@ const login =() => {
       // ...
     });
 }
-export{auth,login}
+export { auth, login }
 
